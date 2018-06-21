ad54c1c668cef1d69c37ca487205232e05ab4199
Luminisc <luminsc@gmail.com>
2018-06-22 01:56:53 +0300

    Ugly users table

--- Gandiva/Controllers/UsersController.cs
+++ Gandiva/Controllers/UsersController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Gandiva.Business;
+using Gandiva.Common;
+using Gandiva.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,20 +11,39 @@ namespace Gandiva.Controllers
 {
     public class UsersController : Controller
     {
-        public ActionResult Index()
+		const int ITEMS_PER_PAGE = 5;
+
+		[HttpGet]
+		public ActionResult Index()
         {
 			ViewBag.TablePartialViewLink = Url.Action("UsersList", "Users", null, Request.Url.Scheme) + "?currentPage=";
 			return View();
         }
 
+		[HttpPost]
+		public ActionResult Index(UserViewModel _model, string action)
+		{
+			switch (action)
+			{
+				case "save":
+					break;
+				case "delete":
+					break;
+				default:
+					break;
+			}
+			ViewBag.TablePartialViewLink = Url.Action("UsersList", "Users", null, Request.Url.Scheme) + "?currentPage=";
+			return View();
+		}
+
+		[HttpGet]
 		public ActionResult UsersList(int currentPage = 0)
 		{
-			//var tasks = TasksService.GetTasksList().Select(task => task.ToViewModel());
-			//var displayedTasks = tasks.Skip(currentPage * ITEMS_PER_PAGE).Take(ITEMS_PER_PAGE);
-			//var model = new HomeViewModel { Tasks = displayedTasks };
-			//ViewBag.Pages = Math.Ceiling(tasks.Count() / (float)ITEMS_PER_PAGE);
-			//return PartialView(model);
-			return PartialView();
+			var users = UserService.GetUsers().Select(e => e.ToViewModel()).ToList();
+			var model = new UsersViewModel { Users = users };
+			var displayedUsers = users.Skip(currentPage * ITEMS_PER_PAGE).Take(ITEMS_PER_PAGE);
+			ViewBag.Pages = Math.Ceiling(users.Count() / (float)ITEMS_PER_PAGE);
+			return PartialView(model);
 		}
 	}
 }
